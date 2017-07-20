daea7b6e9541fbfb052522da08ddb46edabc4a6a
M.Charattrakool <m.charattrakool@gmail.com>
2017-07-20 19:03:47 +0700

    [etc] commit everything

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var Redis = require('ioredis');
 var redis = new Redis();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/',function(req, res){
    res.send("root path"); 
@@ -10,18 +13,51 @@ app.get('/',function(req, res){
 app.get('/data/:key', function(req, res){
     redis.get(req.params.key,function(err,result){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }else{
             res.send(result);
         }
     });
 });
 
+app.get('/data/:namespace/:key', function(req, res){
+    redis.get(req.params.namespace+'_'+req.params.key,function(err,result){
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.send(result);
+        }
+    })
+})
+
 app.get('/data/:key/set/:value', function(req, res){
     redis.set(req.params.key, req.params.value);
     res.send(req.params.value);
 });
 
+app.get('/data/:namespace/:key/set/:value', function(req,res){
+    redis.set(req.params.namespace+'_'+req.params.key, req.params.value)
+    res.send(res);    
+})
+
+app.get('/json/:name', function(req, res){
+    redis.get('json_'+req.params.name, function(err,result){
+        if(err){res.status(500).json({error: err});}
+            else{
+                result = JSON.parse(result);
+                for(var key in result){
+                    console.log(key+':'+result[key])
+                }
+                res.json(result);
+            }
+    })
+})
+
+app.post('/json/:name', function(req, res){
+    console.log(req.body);
+    redis.set('json_'+req.params.name, JSON.stringify(req.body));
+    res.json(req.body);
+})
 
 app.listen(3000,function(){
     console.log('listen on port 3000');
